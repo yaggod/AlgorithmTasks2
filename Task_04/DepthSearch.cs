07ab88adfbf4bd93fd9475d31c6a61efe11fdabd
yaggod <yaggod@yandex.ru>
2024-02-11 02:58:33 +0300

    Finished task 4

---
+++ Task_04/DepthSearch.cs
@@ -0,0 +1,39 @@
+﻿using GraphParser;
+
+namespace Task_04
+{
+	public class DepthSearch
+	{
+		public static List<List<int>> GetComponents(Graph graph)
+		{
+			HashSet<int> visitedNodes = new();
+			List<List<int>> components = new();
+
+			for (int i = 0; i < graph.Size; i++)
+			{
+				if (visitedNodes.Contains(i))
+					continue;
+
+				List<int> component = new();
+				Traverse(graph, i, visitedNodes, component);
+				components.Add(component);
+			}
+
+			return components;
+		}
+
+		private static void Traverse(Graph graph, int currentNode, HashSet<int> visitedNodes, List<int> destination)
+		{
+			if (visitedNodes.Contains(currentNode))
+				return;
+
+			visitedNodes.Add(currentNode);
+			destination.Add(currentNode);
+			foreach(int node in graph.GetAccessibleNodes(currentNode))
+			{
+				Traverse(graph, node, visitedNodes, destination);
+			}
+			
+		}
+	}
+}
---
+++ Task_04/Program.cs
@@ -0,0 +1,24 @@
+﻿using GraphParser;
+
+namespace Task_04
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Graph graph = GraphParser.GraphParser.GetFromFile(@"..\..\..\..\GraphParser\graph.txt", true);
+			Console.WriteLine(graph + "\n\n");
+
+			var components = DepthSearch.GetComponents(graph);
+			using (var stream = File.CreateText(@"..\..\..\result.txt"))
+			{
+				foreach (var component in components)
+				{
+					string stringToWrite = $"There are component with items: [{String.Join("; ", component)}]";
+					stream.WriteLine(stringToWrite);
+					Console.WriteLine(stringToWrite);
+				}
+			}
+		}
+	}
+}
