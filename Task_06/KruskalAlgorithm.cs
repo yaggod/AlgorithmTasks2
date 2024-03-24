3703e6189a6da95859139fcd1dd435575b5767aa
yaggod <yaggod@yandex.ru>
2024-03-24 23:49:24 +0300

    Finished task 6

---
+++ Task_06/KruskalAlgorithm.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using GraphParser;
+
+namespace Task_06
+{
+    public static class KruskalAlgorithm
+    {
+        public static DistancesGraph GetMinimumSpanningTree(DistancesGraph originalGraph)
+        {
+            DistancesGraph result = new(originalGraph.Size);
+            IEnumerable<DistancesGraph.Edge> sortedEdges = originalGraph.GetAllEdges().OrderBy(item => item.Distance);
+            int addedEdgesCount = 0;
+            int currentEdgeIndex = 0;
+
+            while(addedEdgesCount + 1 < originalGraph.Size && currentEdgeIndex < sortedEdges.Count())
+            {
+                DistancesGraph.Edge currentEdge = sortedEdges.ElementAt(currentEdgeIndex++);
+                result.AddConnection(currentEdge);
+                addedEdgesCount++;
+                if (IsInvalidSpanningTreeEdges(result))
+                {
+                    result.RemoveConnection(currentEdge);
+                    addedEdgesCount--;
+                }
+            }
+
+            return result;
+        }
+
+        private static bool IsInvalidSpanningTreeEdges(DistancesGraph result)
+        {
+            return DepthSearch.ContainsCycles(result);
+        }
+
+    }
+}
---
+++ Task_06/Program.cs
@@ -0,0 +1,14 @@
+﻿using GraphParser;
+
+namespace Task_06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DistancesGraph graph = GraphParser.DistancesGraphParser.GetFromFile(@"..\..\..\..\GraphParser\distancesGraph.txt");
+            var result = KruskalAlgorithm.GetMinimumSpanningTree(graph);
+            Console.WriteLine(result);
+        }
+    }
+}
