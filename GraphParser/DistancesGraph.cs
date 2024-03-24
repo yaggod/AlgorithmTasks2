d16ca796f09d33d0ab476587ff62980807fe2c77
yaggod <yaggod@yandex.ru>
2024-03-24 22:37:15 +0300

    Add non-1 distance graph class

---
+++ GraphParser/DistancesGraph.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphParser
+{
+    public class DistancesGraph
+    {
+        private readonly ConnectionData[][] _graphDistancesMatrix;
+
+        public struct ConnectionData
+        {
+            public bool IsConnected {get; }
+            public int Distance { get; }
+            public ConnectionData(bool isConnected, int distance)
+            {
+                IsConnected = isConnected;
+                Distance = distance;
+            }
+
+        }
+
+
+        public int Size
+        {
+            get;
+        }
+
+        public DistancesGraph(int size)
+        {
+            Size = size;
+            _graphDistancesMatrix = new ConnectionData[size][];
+
+            for (int i = 0; i < size; i++)
+            {
+                _graphDistancesMatrix[i] = new ConnectionData[size];
+            }
+        }
+
+        public void AddConnection(int from, int to, int distance)
+        {
+            ConnectionData data = new ConnectionData(true, distance);
+            _graphDistancesMatrix[from][to] = data;
+            _graphDistancesMatrix[to][from] = data;
+
+        }
+
+    }
+}
---
+++ GraphParser/DistancesGraphParser.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphParser
+{
+    public class DistancesGraphParser
+    {
+        public static DistancesGraph GetFromFile(string filePath)
+        {
+            string[] lines = File.ReadAllLines(filePath);
+            int expectedLength = lines.Length;
+            DistancesGraph graph = new DistancesGraph(expectedLength);
+            for (int i = 0; i < expectedLength; i++)
+            {
+                string[] valueStrings = lines[i].Split();
+                if (valueStrings.Length != expectedLength)
+                    throw new InvalidOperationException("Graph matrix can not be non-square");
+
+                for (int j = 0; j < valueStrings.Length; j++)
+                {
+                    bool valueToSet = int.Parse(valueStrings[j]) != 0;
+                    if (!valueToSet)
+                        continue;
+
+                    graph.AddConnection(i, j, valueToSet);
+                }
+            }
+
+            return graph;
+        }
+    }
+}
