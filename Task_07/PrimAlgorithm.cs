477f735a9d5fa7196687d5ce3a8f1187feb671c2
yaggod <yaggod@yandex.ru>
2024-03-25 18:48:02 +0300

    Finished task 7

---
+++ Task_07/PrimAlgorithm.cs
@@ -0,0 +1,41 @@
+﻿using GraphParser;
+
+namespace Task_07
+{
+    public class PrimAlgorithm
+    {
+        internal static DistancesGraph GetMinimumSpanningTree(DistancesGraph originalGraph)
+        {
+            DistancesGraph result = new(originalGraph.Size);
+            HashSet<int> addedNodes = new();
+            List<DistancesGraph.Edge> edgesToHandle = new();
+            addedNodes.Add(0);
+            edgesToHandle.AddRange(originalGraph.GetAccessibleNodesEdges(0));
+
+            while(addedNodes.Count < originalGraph.Size)
+            {
+                edgesToHandle.Sort((first, second) => first.Distance.CompareTo(second.Distance));
+                for (int i = 0; i < edgesToHandle.Count; i++)
+                {
+                    DistancesGraph.Edge edge = edgesToHandle[0];
+                    edgesToHandle.RemoveAt(0);
+                    if (result.ContainsEdge(edge))
+                        continue;
+                    result.AddConnection(edge);
+                    
+                    if (!result.ContainsCycles())
+                    {
+                        addedNodes.Add(edge.SecondNode);
+                        edgesToHandle.AddRange(originalGraph.GetAccessibleNodesEdges(edge.SecondNode));
+                        break;
+                    }
+                    result.RemoveConnection(edge);
+                }
+            }
+
+            return result;
+        }
+        
+        
+    }
+}
---
+++ Task_07/Program.cs
@@ -0,0 +1,14 @@
+﻿using GraphParser;
+
+namespace Task_07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DistancesGraph graph = DistancesGraphParser.GetFromFile(@"..\..\..\..\GraphParser\distancesGraph.txt");
+            var result = PrimAlgorithm.GetMinimumSpanningTree(graph);
+            Console.WriteLine(result);
+        }
+    }
+}
