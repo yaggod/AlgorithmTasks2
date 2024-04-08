9e3189e4a9a9a6bbb736103e021873c93dc99f63
yaggod <yaggod@yandex.ru>
2024-04-08 09:34:07 +0300

    finished task 9

---
+++ Task_09/BellmanFordAlgorithm.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GraphParser;
+
+namespace Task_09
+{
+    internal class BellmanFordAlgorithm
+    {
+        internal static float[] GetShortestDistances(DistancesGraph graph, int originNode)
+        {
+            if (originNode < 0 || originNode >= graph.Size)
+                throw new ArgumentOutOfRangeException(nameof(originNode));
+
+            float[] knownDistances = new float[graph.Size];
+            HashSet<int> visitedNodes = new HashSet<int>();
+            for (int i = 0; i < graph.Size; i++)
+                knownDistances[i] = (i == originNode) ? 0 : float.PositiveInfinity;
+
+
+            for(int i = 0; i < graph.Size; i ++)
+                for(int startingNode = 0; startingNode < graph.Size; startingNode++)
+                {
+                    IEnumerable<int> accessibleNodes = graph.GetAccessibleNodes(startingNode);
+                    foreach(int endingNode in accessibleNodes)
+                    {
+                        int distance = graph.GetConnection(startingNode, endingNode).Distance;
+                        knownDistances[endingNode] = Math.Min(knownDistances[endingNode], knownDistances[startingNode] + distance);
+                    }
+                }
+            return knownDistances;
+        }
+
+    }
+}
