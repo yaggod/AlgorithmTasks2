69143945e629c3bf9e457853622ae5d941c0ff46
yaggod <yaggod@yandex.ru>
2024-04-09 18:58:46 +0300

    minor changes

--- Task_10/Program.cs
+++ Task_10/Program.cs
@@ -7,7 +7,15 @@ namespace Task_10
         static void Main(string[] args)
         {
             Graph graph = GraphParser.GraphParser.GetFromFile(@"..\..\..\..\GraphParser\graph.txt", true);
-            Console.WriteLine(String.Join(" -> ", EulerianCyclesFinder.FindEulerianCycle(graph).Select(item => item + 1)));
+            Console.WriteLine(graph);
+            try
+            {
+                Console.WriteLine(String.Join(" -> ", EulerianCyclesFinder.FindEulerianCycle(graph).Select(item => item + 1)));
+            }
+            catch (ArgumentException exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
         }
     }
 }
