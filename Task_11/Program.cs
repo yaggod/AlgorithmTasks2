8360d1188ba53fc22c29c4978f28df3fa4f24802
yaggod <yaggod@yandex.ru>
2024-04-15 19:50:53 +0300

    Finished task 11

---
+++ Task_11/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Task_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "AABAACAADAABAAABAA";
+            string pattern = "AABA";
+            var result = FiniteAutomataSearch.FindAllOccurances(text, pattern);
+            Console.WriteLine(String.Join(" ", result));
+        }
+    }
+}
