0b9ea99a51cf10e0f716a86fb12205d45fded550
yaggod <yaggod@yandex.ru>
2024-04-23 04:50:53 +0300

    implemented prefix function

---
+++ Task_12/KnuthMorrisPrattAlgorithm.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_12
+{
+    internal class KnuthMorrisPrattAlgorithm
+    {
+        public static List<int> FindAllOccurances(string text, string pattern)
+        {
+            List<int> result = new();
+
+            return result;
+        }
+
+        public static int[] PrefixFunction(string text)
+        {
+            int[] result = new int[text.Length];
+            for(int i = 1; i < result.Length; i++)
+            {
+                int currentPrefixWeight = result[i - 1];
+                while(currentPrefixWeight > 0 && text[i] != text[currentPrefixWeight])
+                {
+                    currentPrefixWeight = result[currentPrefixWeight - 1];
+                }
+                if (text[i] == text[currentPrefixWeight])
+                    currentPrefixWeight++;
+                result[i] = currentPrefixWeight;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Task_12/Program.cs
@@ -0,0 +1,17 @@
+﻿using Task_12;
+
+namespace Task_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string text = File.ReadAllText((@"..\..\..\input.txt"));
+            string pattern = "AABA";
+            //var result = KnuthMorrisPrattAlgorithm.FindAllOccurances(text, pattern);
+            var result = KnuthMorrisPrattAlgorithm.PrefixFunction("abcabcd");
+			Console.WriteLine(String.Join(" ", result));
+        }
+    }
+}
