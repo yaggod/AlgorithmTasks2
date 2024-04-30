90beac009f4e85fea031821dc502454f1f39ae9c
yaggod <yaggod@yandex.ru>
2024-04-30 13:29:25 +0300

    cleanup

--- Task_11/FiniteAutomataSearch.cs
+++ Task_11/FiniteAutomataSearch.cs
@@ -1,72 +1,66 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection.Metadata.Ecma335;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Task_11
+﻿namespace Task_11
 {
-    internal class FiniteAutomataSearch
-    {
-        public static List<int> FindAllOccurrences(string data, string pattern)
-        {
-            List<int> result = new();
-            int totalStatesCount = pattern.Length;
-            Dictionary<char, int[]> table = CreateTable(data, pattern);
+	internal class FiniteAutomataSearch
+	{
+		public static List<int> FindAllOccurrences(string data, string pattern)
+		{
+			List<int> result = new();
+			int totalStatesCount = pattern.Length;
+			Dictionary<char, int[]> table = CreateTable(data, pattern);
 
-            int currentState = 0;
-            for(int i = 0; i < data.Length; i++)
-            {
-                currentState = table[data[i]][currentState];
-                if (currentState == totalStatesCount)
-                    result.Add(i + 1 - totalStatesCount);
-            }
+			int currentState = 0;
+			for (int i = 0; i < data.Length; i++)
+			{
+				currentState = table[data[i]][currentState];
+				if (currentState == totalStatesCount)
+					result.Add(i + 1 - totalStatesCount);
+			}
 
-            return result;
-        }
+			return result;
+		}
 
-        private static Dictionary<char, int[]> CreateTable(string data, string pattern)
-        {
-            Dictionary<char, int[]> result = new();
-            int totalStatesCount = pattern.Length;
-            foreach(char @char in data)
-            {
-                if (!result.ContainsKey(@char))
-                    result.Add(@char, new int[totalStatesCount + 1]);
-            }
+		private static Dictionary<char, int[]> CreateTable(string data, string pattern)
+		{
+			Dictionary<char, int[]> result = new();
+			int totalStatesCount = pattern.Length;
+			foreach (char @char in data)
+			{
+				if (!result.ContainsKey(@char))
+					result.Add(@char, new int[totalStatesCount + 1]);
+			}
 
-            for (int currentState = 0; currentState < totalStatesCount + 1; currentState++)
-            {
-                foreach (char @char in result.Keys)
-                {
-                    result[@char][currentState] = GetNextState(pattern, currentState, @char);
-                }
-            }
-            return result;
-        }
+			for (int currentState = 0; currentState < totalStatesCount + 1; currentState++)
+			{
+				foreach (char @char in result.Keys)
+				{
+					result[@char][currentState] = GetNextState(pattern, currentState, @char);
+				}
+			}
 
-        private static int GetNextState(string pattern, int currentState, char nextCharacter)
-        {
-            int totalStatesCount = pattern.Length;
-            if (currentState < totalStatesCount && pattern[currentState] == nextCharacter)
-                return currentState + 1;
+			return result;
+		}
+
+		private static int GetNextState(string pattern, int currentState, char nextCharacter)
+		{
+			int totalStatesCount = pattern.Length;
+			if (currentState < totalStatesCount && pattern[currentState] == nextCharacter)
+				return currentState + 1;
 
 
-            for (int prefixState = currentState; prefixState > 0; prefixState--)
-            {
-                if (pattern[prefixState - 1] == nextCharacter)
-                {
-                    int i;
-                    for (i = 0; i < prefixState - 1; i++)
-                        if (pattern[i] != pattern[currentState - prefixState + 1 + i])
-                            break;
-                    if (i == prefixState - 1)
-                        return prefixState;
-                }
-            }
+			for (int prefixState = currentState; prefixState > 0; prefixState--)
+			{
+				if (pattern[prefixState - 1] == nextCharacter)
+				{
+					int i;
+					for (i = 0; i < prefixState - 1; i++)
+						if (pattern[i] != pattern[currentState - prefixState + 1 + i])
+							break;
+					if (i == prefixState - 1)
+						return prefixState;
+				}
+			}
 
-            return 0;
-        }
-    }
+			return 0;
+		}
+	}
 }
