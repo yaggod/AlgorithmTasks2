415ac9d7b96a98ab074653a11d6943ceade137c8
yaggod <yaggod@yandex.ru>
2024-05-19 15:22:28 +0300

    Finished task 15

---
+++ Task_15/GraphColorizer.cs
@@ -0,0 +1,35 @@
+﻿using GraphParser;
+
+namespace Task_15
+{
+	internal class GraphColorizer
+	{
+		public static int[] Colorize(Graph graph)
+		{
+			List<int> allColors = new List<int>();
+			int[] graphColors = new int[graph.Size];
+			for (int currentNode = 0; currentNode < graph.Size; currentNode++)
+			{
+				foreach (int color in allColors)
+				{
+					bool isCorrectNode = true;
+					foreach (int secondNode in graph.GetAccessibleNodes(currentNode))
+						if (graphColors[secondNode] == color)
+							isCorrectNode = false;
+					if (!isCorrectNode)
+					{
+						graphColors[currentNode] = color;
+						break;
+					}
+				}
+				if (graphColors[currentNode] == 0)
+				{
+					int newColor = allColors.Count() + 1;
+					graphColors[currentNode] = newColor;
+					allColors.Add(newColor);
+				}
+			}
+			return graphColors;
+		}
+	}
+}
---
+++ Task_15/Program.cs
@@ -0,0 +1,14 @@
+﻿using GraphParser;
+
+namespace Task_15
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+            Graph graph = GraphParser.GraphParser.GetFromFile(@"..\..\..\..\GraphParser\graph1.txt");
+			var result = GraphColorizer.Colorize(graph);
+			
+		}
+	}
+}
