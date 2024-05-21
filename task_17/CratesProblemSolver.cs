d993a2c51e3f42f819301f08e6644597b0991df2
yaggod <yaggod@yandex.ru>
2024-05-21 07:55:16 +0300

    Finished task 17

---
+++ task_17/CratesProblemSolver.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_17
+{
+	public static class CratesProblemSolver
+	{
+		public static int GetCratesCount(int[] itemsSizes, int crateSize)
+		{
+			var sortedItemsSizes = itemsSizes.OrderByDescending(x => x);
+			List<int> crates = new();
+
+			foreach(var item in sortedItemsSizes)
+			{
+				bool placedAny = false;
+				for(int i = 0; i < crates.Count; i++)
+				{
+					if (crates[i] + item <= crateSize)
+					{
+						crates[i] += item;
+						placedAny = true;
+						break;
+					}
+				}
+
+				if (!placedAny)
+					crates.Add(item);
+			}
+
+			return crates.Count;
+
+		}
+	}
+}
