3c9cbba9948d2144a978a618d8fad209f3b71909
yaggod <yaggod@yandex.ru>
2024-05-21 07:55:30 +0300

    Finished task 18

---
+++ task_18/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Task_18
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int itemsCount = 10;
+			Random random = new();
+			List<int> items = new();
+			for (int i = 0; i < itemsCount; i++)
+			{
+				items.Add(random.Next(0, 100));
+			}
+
+			var result = SubsetSumGreedySolver.GetPossibleClosestSumSubset(items, 250);
+			var sum = result.Sum();
+		}
+	}
+}
---
+++ task_18/SubsetSumGreedySolver.cs
@@ -0,0 +1,24 @@
+﻿namespace Task_18
+{
+	public static class SubsetSumGreedySolver
+	{
+		public static List<int> GetPossibleClosestSumSubset(IEnumerable<int> numbers, int target)
+		{
+			var sortedNumbers = numbers.Order();
+			List<int> resultItems = new List<int>();
+			int sum = 0;
+			foreach (var number in sortedNumbers)
+			{
+				if (number + sum <= target)
+				{
+					resultItems.Add(number);
+					sum += number;
+				}
+				else
+					break;
+			}
+
+			return resultItems;
+		}
+	}
+}
