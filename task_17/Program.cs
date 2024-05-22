6b8d60290c2e7d12f7f1f3c437c7e3f94e235f5f
yaggod <yaggod@yandex.ru>
2024-05-22 13:42:43 +0300

    Changed main method

--- task_17/Program.cs
+++ task_17/Program.cs
@@ -4,7 +4,13 @@
 	{
 		static void Main(string[] args)
 		{
-			
+			List<int> items = new();
+			Random random = new Random();
+			for (int i = 0; i < 10; i++)
+			{
+				items.Add(random.Next(1, 8));
+			}
+			var result = CratesProblemSolver.GetCratesCount(items.ToArray(), 10);
 		}
 	}
 }
